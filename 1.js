987a35e30a88a3cac4261c16a25f9382241d0533
Sujina_BH <shalesh@qwinix.io>
2016-10-25 13:11:19 +0530

    msgs

---
+++ 1.js
@@ -0,0 +1,96 @@
+﻿
+
+
+														var index = 0;
+	slideShow();
+	
+	function slideShow(){
+	
+	var slide1=document.getElementsByClassName("slideimage");
+	for(var i=0;i<slide1.length;i++){
+		slide1[i].style.display="none";
+	
+
+	}
+
+	
+	var slide2=document.getElementsByClassName("slideimage");
+	
+	if(index+1>slide1.length){
+		index=0;
+	}
+
+	slide2[index].style.display="block";
+	
+	index++;
+	setTimeout(slideShow, 4000);
+
+	
+}
+
+
+
+
+
+													$('#homemenu').click(function(){
+														debugger;
+															$('#homepage').show();
+															$('#aboutpage').hide();
+															return false;
+													});
+
+													$('#aboutmenu').click(function(){
+														debugger;
+															$('#homepage').hide();
+															$('#aboutpage').show();
+															return false;
+													});
+
+													$('.bilgates').click(function(){
+														$('.bilgatesText').show();
+														$('.zuckerbergText').hide();
+														$('.narayanText').hide();
+														return false;
+													});
+
+														$('.zuckerberg').click(function(){
+														$('.bilgatesText').hide();
+														$('.zuckerbergText').show();
+														$('.narayanText').hide();
+														return false;
+													});
+
+													$('.narayan').click(function(){
+														$('.bilgatesText').hide();
+														$('.zuckerbergText').hide();
+														$('.narayanText').show();
+														return false;
+													});
+													
+														
+
+
+														
+
+
+													
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,101 @@
+﻿
+
+
+														var index = 0;
+	slideShow();
+	
+	function slideShow(){
+	
+	var slide1=document.getElementsByClassName("slideimage");
+	for(var i=0;i<slide1.length;i++){
+		slide1[i].style.display="none";
+	
+
+	}
+
+	
+	var slide2=document.getElementsByClassName("slideimage");
+	
+	if(index+1>slide1.length){
+		index=0;
+	}
+
+	slide2[index].style.display="block";
+	
+	index++;
+	setTimeout(slideShow, 1000);
+
+	
+}
+
+
+
+
+
+													$('#homemenu').click(function(){
+														debugger;
+															$('#homepage').show();
+															$('#aboutpage').hide();
+															$('#image').hide();
+															$('#button').hide();
+
+															return false;
+													});
+
+													$('#aboutmenu').click(function(){
+														debugger;
+															$('#homepage').hide();
+															$('#aboutpage').show();
+															$('#image').show();
+															$('#button').show();
+															return false;
+													});
+
+													$('.bilgates').click(function(){
+														$('.bilgates').show();
+														$('.zuckerberg').hide();
+														$('.narayan').hide();
+														return false;
+													});
+
+														$('.zuckerberg').click(function(){
+														$('.bilgates').hide();
+														$('.zuckerberg').show();
+														$('.narayan').hide();
+														return false;
+													});
+
+													$('.narayan').click(function(){
+														$('.bilgates').hide();
+														$('.zuckerberg').hide();
+														$('.narayan').show();
+														return false;
+													});
+													
+														
+
+
+														
+
+
+													
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
